768405b97a7f061fc892fdc6a18084e0e3b09133
Qing Tang <qingt@uci.edu>
2017-08-22 16:49:53 -0700

    Create 155.py

---
+++ 155.py
@@ -0,0 +1,50 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        # if self.min == None:
+        #     self.stack.append((x, x))
+        min = self.getMin()
+        if min == None or min > x:
+            min = x
+        self.stack.append((x, min))
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
